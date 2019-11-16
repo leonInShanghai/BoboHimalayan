a035b052449164ee35202a43e4fb1292e3aecc19
Leon <646026023@qq.com>
2019-11-16 12:33:22 +0800

    初步集成喜马拉雅sdk 第一次接触到混淆

--- app/src/main/java/com/bobo/himalayan/MainActivity.java
+++ app/src/main/java/com/bobo/himalayan/MainActivity.java
@@ -1,13 +1,52 @@
 package com.bobo.himalayan;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String,String> map = new HashMap<>();
+
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(@Nullable CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+
+                if (categories != null) {
+                    int size = categories.size();
+                    Log.e(TAG,"categories size ---->"+categories);
+
+                    //增强for循环快捷键 iter
+                    for (Category category : categories) {
+                        Log.e(TAG,"category -->"+category.getCategoryName());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(TAG,"error code-- "+i+"error message ==>"+s);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/bobo/himalayan/base/BaseApplication.java
@@ -0,0 +1,38 @@
+package com.bobo.himalayan.base;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+
+/**
+ * Created by 求知自学网 on 2019/11/16. Copyright © Leon. All rights reserved.
+ * Functions:
+ */
+public class BaseApplication extends Application {
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CommonRequest mXimalaya = CommonRequest.getInstanse();
+        if (DTransferConstants.isRelease){
+            String mAppSecret = "8646d66d6abe2efd14f2891f9fd1c8af";
+            mXimalaya.setAppkey("9f9ef8f10bebeaa83e71e62f935bede8");
+            mXimalaya.setPackid("com.app.test.android");
+            mXimalaya.init(this, mAppSecret);
+            Log.e("BaseApplication","isRelease");
+        }else{
+            String mAppSecret = "0a09d7093bff3d4947a5c4da0125972e";
+            mXimalaya.setAppkey("f4d8f65918d9878e1702d49a8cdf0183");
+            mXimalaya.setPackid("com.ximalaya.qunfeng");
+            mXimalaya.init(this, mAppSecret);
+            Log.e("BaseApplication","isDebug");
+        }
+
+        }
+}
